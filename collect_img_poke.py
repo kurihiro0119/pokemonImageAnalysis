50a0b765b1f7c64ebc8808c3297871c920987ac0
kurihiro0119 <54324711+kurihiro0119@users.noreply.github.com>
2020-09-12 15:51:23 +0900

    src commit

---
+++ collect_img_poke.py
@@ -0,0 +1,43 @@
+import os
+import glob
+import random
+import shutil
+from icrawler.builtin import GoogleImageCrawler
+
+#画像を保存するルートディレクトリ
+root_dir = 'pokemon/'
+#魚画像検索キーワードリスト
+pokemonnames = ['ピカチュウ','ゼニガメ','ヒトカゲ','フシギダネ','カビゴン']
+
+#収集画像データ数
+data_count = 100
+
+for pokemonname in pokemonnames:
+    crawler = GoogleImageCrawler(storage={'root_dir':root_dir + pokemonname + '/train'})
+    
+    filters = dict(
+        size = 'large',
+        type = 'photo'
+    )
+    
+    #クローリングの実行
+    crawler.crawl(
+        keyword=pokemonname,
+        filters=filters,
+        max_num=data_count
+    )
+    
+    #前回実行時のtestディレクトリが存在する場合、ファイルをすべて削除する
+    
+    if os.path.isdir(root_dir + pokemonname + '/test'):
+        shutil.rmtree(root_dir + pokemonname + '/test')
+    os.makedirs(root_dir + pokemonname + '/test')
+    
+    #ダウンロードファイルのリストを取得
+    filelist = glob.glob(root_dir + pokemonname + '/train/*')
+    #ダウンロード数の2割をtestデータとして抽出
+    test_ratio = 0.2
+    testfiles = random.sample(filelist, int(len(filelist) * test_ratio))
+    
+    for testfile in testfiles:
+        shutil.move(testfile, root_dir + pokemonname + '/test/')
---
+++ predictions_poke_test.py
@@ -0,0 +1,53 @@
+import glob
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+from msrest.authentication import ApiKeyCredentials
+import requests
+import json
+import urllib.parse
+import urllib.request
+
+base_url = 'https://hiku-fish-model.cognitiveservices.azure.com/customvision/v3.0/Prediction/5fff1e17-d352-4717-a411-ff2ba1dc20d7/classify/iterations/Iteration1/image'
+prediction_key = 'b867ca7a5601437e9d9654534360c522'
+
+poke_root_dir = 'pokemon/'
+# 検証対象のポケモン名一覧
+pokemonnames = ['ピカチュウ','ゼニガメ','ヒトカゲ','フシギダネ','カビゴン']
+
+for pokename in pokemonnames:
+    testfiles = glob.glob(poke_root_dir + pokename + '/test/*')
+    data_count = len(testfiles)
+    true_count = 0
+    
+    for testfile in testfiles:
+        headers = {
+            'Content-Type': 'application/json',    
+            'Prediction-Key': prediction_key
+        }
+        
+        params = {}
+        predicts = {}
+        data = open(testfile, 'rb').read()
+        response = requests.post(base_url, headers=headers, params=params, data=data)
+        results = json.loads(response.text)
+        
+        try:
+            # 予測結果のタグの数だけループ　…③
+            for prediction in results['predictions']:
+                # 予測したポケモンとその確率を紐づけて格納　…④
+                predicts[prediction['tagName']] = prediction['probability']
+            # 一番確率の高いポケモンを予測結果として選択　…⑤
+            prediction_result = max(predicts, key=predicts.get)
+        
+            # 予測結果が合っていれば正解数を増やす
+            if pokename == prediction_result:
+                true_count += 1
+
+        #画像サイズ > 6MB だとCustom Vision の制限にひっかりエラーが出るまで握り潰し
+        except KeyError:
+            data_count -= 1
+            continue
+    
+    # 正解率の算出
+    accuracy = (true_count / data_count) * 100
+    print('ポケモン名:' + pokename)
+    print('正解率:' + str(accuracy) + '%')
